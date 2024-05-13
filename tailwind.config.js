fb6f5fb5bc8113d0050f6b570a7e2970c144109c
fawwazhumam <mfawwazhumam@gmail.com>
2024-05-13 23:30:10 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html,js}"], 
+  theme: {
+    extend: {
+      colors: {
+        primary: "#369A69",
+        secondary: "#474747",
+      },
+    },
+  },
+  plugins: [],
+};
