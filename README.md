18d578757cb8319e153b7b103fb4694cead8b698
muhafiq <muhafiq18@gmail.com>
2024-05-14 20:50:55 +0700

    add:pages and routing  & fix:config and structure files

---
+++ README.md
@@ -0,0 +1,15 @@
+# SETUP PROJECT
+
+Install semua dependency
+
+```bash
+npm install
+```
+
+Kemudian running tailwindnya
+
+```bash
+npm run dev
+```
+
+Running projectnya bisa pake `live server`
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./**/*.{html,js}"], 
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: "#369A69",
         secondary: "#474747",
       },
+    },
+    screens: {
+      xl: { max: "1280px" },
+      lg: { max: "1024px" },
+      md: { max: "768px" },
+      sm: { max: "640px" },
     },
   },
   plugins: [],
